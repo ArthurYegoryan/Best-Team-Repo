7e9b448dc267d820dc04254e46d2f5206e2c44de
Arev <vardanyanarevik23@gmail.com>
2023-11-16 15:15:54 +0400

    Select is working,by printing flag of selected counrty

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 
+let selectElement = document.getElementById("country")
 let country = fetch( 'https://restcountries.com/v3.1/all')
 .then(res => res.json())
 .then(data =>{
-    let select = "";
     console.log(data)
     data.map((countryName)=>{
-        select += `
-            <option>${countryName["name"]["common"]}</option>
-            `
-    });
-    document.getElementById("country").innerHTML = select
-    
-} )
+        let opt = document.createElement("option");
+        opt.value = countryName["name"]["common"];
+        opt.text = countryName["name"]["common"];
+        selectElement.appendChild(opt)
 
+        
+    })
+})
+.catch(error => console.error("Error fetching data",error))
 
-
+selectElement.addEventListener("change",()=>{
+    console.log(selectElement.value)
+        fetch(`https://restcountries.com/v3.1/name/${selectElement.value}`).
+        then(res => res.json()).
+        then(data =>{
+            let imgData = data[0].flags.svg
+            let div = '';
+            console.log(data[0].flags.svg)
+            div += `<img src="${imgData}" alt="">`
+            document.getElementById("image").innerHTML = div
+        })
+})
