ba4b6b1d8bf6bd18c5e4707dd93094e71353edb4
Arev <vardanyanarevik23@gmail.com>
2023-11-16 23:33:50 +0400

    Selection 'All' is working

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
 
-let selectElement = document.getElementById("country")
+let selectElement = document.getElementById("country");
+let searchInput = document.getElementById("searchInput");
+let countryUl = document.getElementById("countriesList")
+let countryDet = document.createElement("div")
+
 let country = fetch( 'https://restcountries.com/v3.1/all')
 .then(res => res.json())
 .then(data =>{
-    console.log(data)
     data.map((countryName)=>{
         let opt = document.createElement("option");
         opt.value = countryName["name"]["common"];
         opt.text = countryName["name"]["common"];
         selectElement.appendChild(opt)
-
+        
         
     })
+    console.log(data)
 })
 .catch(error => console.error("Error fetching data",error))
 
 selectElement.addEventListener("change",()=>{
-    console.log(selectElement.value)
         fetch(`https://restcountries.com/v3.1/name/${selectElement.value}`).
         then(res => res.json()).
         then(data =>{
+            console.log(data)
             let imgData = data[0].flags.svg
             let div = '';
-            console.log(data[0].flags.svg)
             div += `<img src="${imgData}" alt="">`
-            document.getElementById("image").innerHTML = div
+            countryDet.innerHTML = div
         })
 })
+
+
